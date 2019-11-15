02b18f9adadd4b88876543ba42ec8fcff8df74d7
nicolasdebr <nicolas.debruyker@gmail.com>
2019-11-15 11:01:05 +0100

    Premier projet

---
+++ file_rouge/Charte/green.js
@@ -0,0 +1,14 @@
+var guitare = document.getElementById("imgguit");
+var batterie = document.getElementById("imgbat");
+
+var img = new image();
+
+image.onload = function()
+{
+
+guitare.src = image.src
+
+
+}
+
+image.src ="BODY/CATEGORIES_ROLLOVER_guitare";
